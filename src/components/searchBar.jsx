a2a2322cb3d7f5cc265ba7c1559fe8db86931300
Luzbel061 <benitezmaldonadocristopherartu@gmail.com>
2024-04-17 14:08:12 -0700

    Agregue tres nuevos componentes con funcionalidad basica

---
+++ src/components/searchBar.jsx
@@ -0,0 +1,33 @@
+import { useState} from "react"
+
+function searchBar({onSumit}){
+    const [term, setTerm] = useState('')
+
+    const handleFormSubmit = (event)=>{
+        event.preventDefault()
+        console.log('Necesito decirle al componenete padre sobre los datos')
+        onsubmit(term)
+    }
+
+    const handleChange = (event) =>{
+        console.log(event.target.value)
+        setTerm(event.target.value)
+    }
+
+    const handleClick = () =>{
+        onsubmit(term)
+    }
+
+    return(
+        <div>
+          <h1> Search Bar </h1>
+          <form onSubmit={handleFormSubmit}>
+            confirma tu busqueda: {term}
+            <input onChange={handleChange} value={term}/>
+          </form>
+          <buttom onClick={handleClick}>Click me!</buttom>
+        </div>
+    )
+}
+
+export default searchBar
